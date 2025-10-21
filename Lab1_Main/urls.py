7c99d2c3521adb76e954d1768a7f295ee215de16
alexlewington1 <s311480@uos.ac.uk>
2025-10-21 14:41:38 +0100

    updated main URLS

--- Lab1_Main/urls.py
+++ Lab1_Main/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import Lab1_Working
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('Lab1_Working.urls')),
 ]
